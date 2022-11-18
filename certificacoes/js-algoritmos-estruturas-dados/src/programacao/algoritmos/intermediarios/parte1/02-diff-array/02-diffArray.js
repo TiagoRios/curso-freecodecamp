f3423cab21426edaf92608328130214685de9221
Tiago Rios <Tiago.kom@gmail.com>
2022-11-17 22:55:20 -0300

    Algoritmos intermediários - PARTE 1 - 02-Diferença simétrica - concluído.

--- certificacoes/js-algoritmos-estruturas-dados/src/programacao/algoritmos/intermediarios/parte1/02-diff-array/02-diffArray.js
+++ certificacoes/js-algoritmos-estruturas-dados/src/programacao/algoritmos/intermediarios/parte1/02-diff-array/02-diffArray.js
@@ -1,9 +1,52 @@
+//Minha solução utilizava 2 loops for. Um horror só de pensar.
+//Solução refatorada e é parecida com a solução 1.
+
+//O temop de processamento das funções e variações devem analizadas com cautela.
+
+/**
+ * Função que devolve a diferença simetrica entre dois arrays.
+ * 
+ * @param {*} arr1 Primeiro array para comparar
+ * @param {*} arr2 Segundo array para comparar
+ * @returns Array com os elementos não presentes no outro array 
+ */
+//1ª solução do site - Melhorada.
 function diffArray(arr1, arr2) {
   const newArr = [];
+  //Arrays mesclados com atribuição de desestruturação. MASSA Demais!
+  for (const element of [...arr1, ...arr2]) {
+    //Se não possuir os elementos no arr1 ou arr2.
+    //Então insere no Array
+    if (!arr1.includes(element) || !arr2.includes(element)) {
+      newArr.push(element)
+    }
+  }
   return newArr;
 }
 
-diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+/****************************************
+            
+             Outras soluções
+
+ ****************************************/
+
+//2ª solução do site.
+function diffArray2(arr1, arr2) {
+  return arr1
+    .concat(arr2)
+    .filter(a => !arr1.includes(a) || !arr2.includes(a));
+}
+
+//3ª solução do site. Variação da solução 2.
+
+//4ª solução do site.
+function diffArray4(arr1, arr2) {
+  const difference = new Set(arr1);
+  arr2.forEach((ele) =>
+    difference.has(ele) ? difference.delete(ele) : difference.add(ele)
+  );
+  return Array.from(difference);
+}
 
 module.exports = {
   diffArray
