81b93d7fb1d6cb7fdf61d7d4cf8d875bdf740b9a
Tiago Rios <Tiago.kom@gmail.com>
2022-11-18 15:13:05 -0300

    Algoritmos intermediários - PARTE 1 - 03-Procurar e destruir - concluído.

--- certificacoes/js-algoritmos-estruturas-dados/src/programacao/algoritmos/intermediarios/parte1/03-destroyer/03-destroyer.js
+++ certificacoes/js-algoritmos-estruturas-dados/src/programacao/algoritmos/intermediarios/parte1/03-destroyer/03-destroyer.js
@@ -1,8 +1,35 @@
+/**
+ * Função que remove do Array os argumentos extras passados dentro dela.
+ * 
+ * Não modifica o Array orginal.
+ * 
+ * @param {*} arr Array a ser pesquisado. 
+ * @returns Array depois de removido os valores extras.
+ */
 function destroyer(arr) {
-  return arr;
+  let remover = Object.values(arguments).slice(1)
+  return arr.filter(a => remover.indexOf(a) === -1)
 }
 
-//destroyer [1, 2, 3, 1, 2, 3], 2, 3);
+/****************************************
+            
+             Outras soluções
+
+ ****************************************/
+
+//1ª solução do site. Nem compensa. Muito verbosa e difícil de manter
+//2ª solução do site. Parecida com a minha.
+function destroyer2(arr) {
+  const valsToRemove = Array.from(arguments).slice(1);
+  return arr.filter(function(val) {
+    return !valsToRemove.includes(val);
+  });
+}
+
+//3ª solução do site.
+function destroyer3(arr, ...valsToRemove) {
+  return arr.filter(elem => !valsToRemove.includes(elem));
+}
 
 module.exports = {
   destroyer
