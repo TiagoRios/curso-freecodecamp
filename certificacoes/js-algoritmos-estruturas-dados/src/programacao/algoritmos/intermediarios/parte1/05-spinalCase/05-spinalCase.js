79c44a88e57be7a856dc3fd33b0d4bb046844b07
Tiago Rios <Tiago.kom@gmail.com>
2022-11-24 21:30:34 -0300

    Algoritmos intermediários - PARTE 1 - 05-Hifenizar - concluído.

--- certificacoes/js-algoritmos-estruturas-dados/src/programacao/algoritmos/intermediarios/parte1/05-spinalCase/05-spinalCase.js
+++ certificacoes/js-algoritmos-estruturas-dados/src/programacao/algoritmos/intermediarios/parte1/05-spinalCase/05-spinalCase.js
@@ -1,8 +1,45 @@
+/**
+ * Função que retorna outra string formatada com traços.
+ * 
+ * @param {*} str String para analise  
+ * @returns nova String separada por '-' traço.  
+ */
 function spinalCase(str) {
-  return str;
+  return str
+    .split(/\W|_|(?=[A-Z])/)//LookAHead positivo.
+    .join('-')
+    .toLowerCase();
 }
 
-spinalCase('This Is Spinal Tap');
+/****************************************
+            
+             Outras soluções
+
+ ****************************************/
+
+//1ª solução do site.
+function spinalCase1(str) {
+  let regex = /\s+|_+/g;
+  str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+  return str.replace(regex, "-").toLowerCase();
+}
+
+//2ª solução do site.
+function spinalCase2(str) {
+  str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+  return str
+  .toLowerCase()
+  //.split(/(?:_| )+/) //problemas com Lint
+  .join("-");
+}
+
+//3ª solução do site.
+function spinalCase3(str) {
+  return str
+    .split(/\s|_|(?=[A-Z])/)
+    .join("-")
+    .toLowerCase();
+}
 
 module.exports = {
   spinalCase
