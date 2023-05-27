46c6fd7d24f8f97f5f2d35afdcf671a1742fafb9
Tiago Rios <tiago_pc9@hotmail.com>
2023-05-27 18:15:39 -0300

    Algoritmos intermediários - PARTE 3 - 15-drop-Elements - Concluído.

---
+++ certificacoes/js-algoritmos-estruturas-dados/src/programacao/algoritmos/intermediarios/parte3/15-drop-Elements/15-dropElements.js
@@ -0,0 +1,23 @@
+/**
+ * Remove os elementos que satisfazem a callback.
+ * 
+ * Esta função não altera o array original.
+ * 
+ * @param {Array} arr Array que deve ser removido elementos.
+ * @param {Function} func callback que define quais elementos devem ser excluidos. 
+ * @returns Array filtrado.
+ */
+function dropElements(arr, func) {
+  for (let i = 0; i < arr.length; i++) {
+    if(func(arr[i])){
+      return arr.slice(i);
+    }      
+  }
+  return [];
+}
+
+console.log(dropElements([1, 2, 3,2,1], function (n) { return n >= 3; }));
+
+module.exports = {
+  dropElements
+}
