cf8f3248c17cab9b3de92527f70607a38ac570f2
Tiago Rios <tiago_pc9@hotmail.com>
2023-08-24 23:44:40 -0300

    Redux: contador utilizando redux.

---
+++ certificacoes/bibliotecas-desenvolvimento-frontend/cursos/redux/src/contadorRedux.js
@@ -0,0 +1,38 @@
+// recomenda-se o RTK. createStore() foi substituído por configureStore()
+import { legacy_createStore as createStore } from 'redux'
+
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+
+const counterReducer = (state = 0, action) => {
+    switch (action.type) {
+        case INCREMENT:
+            return state + 1;
+        case DECREMENT:
+            return state - 1;
+        default:
+            return state;
+    }
+};
+
+const incAction = () => {
+    return {
+        type: INCREMENT
+    }
+};
+
+const decAction = () => {
+    return {
+        type: DECREMENT
+    }
+};
+
+const store = Redux.createStore(counterReducer)
+
+console.log("inicial: ", store.getState());
+store.dispatch(incAction());
+console.log("incr: ", store.getState());
+store.dispatch(decAction());
+console.log("decr: ", store.getState());
+store.dispatch(decAction());
+console.log("decr: ", store.getState());
